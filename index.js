fa063b913dc2648436f336abd620e115ec618024
wangqun6 <wangqun6@jd.com>
2017-02-04 16:24:08 +0800

    修改

--- index.js
+++ index.js
@@ -5,7 +5,27 @@
 let through = require('through2');
 let gutil = require('gulp-util');
 let PluginError = gutil.PluginError;
+let pluginName = 'gulp-css-img-sprite';
+let css_img_sprite = require('./lib/css-img-sprite');
 
-module.exports = function () {
+module.exports = function (obj) {
+    return through.obj(function (file, encoding, cb) {
+        if (file.isStream()) {
+            this.emit('error', new PluginError(pluginName, 'Streams are not supported!'));
+            return cb();
+        }
 
+        if (file.isBuffer()) {
+            let content = css_img_sprite(file.contents.toString(), {
+                cssSrc: file.path
+            });
+            file.contents = Buffer.from(content);
+        }
+
+        // make sure the file goes through the next gulp plugin
+        this.push(file);
+
+        // tell the stream engine that we are done with this file
+        cb();
+    });
 };
