d763a9a5345b7169164c2d079f41cb5e03ae95f0
wangqun6 <wangqun6@jd.com>
2017-02-04 15:55:37 +0800

    添加依赖文件

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
  * Created by wangqun6 on 2017/1/17.
  */
 
+let through = require('through2');
+let gutil = require('gulp-util');
+let PluginError = gutil.PluginError;
 
+module.exports = function () {
+
+};
